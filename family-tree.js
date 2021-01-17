7acbc2387de2ada7a71813b04af4f2afc5b875c3
arjanmitra <arjanmitra@hotmail.com>
2021-01-16 19:16:24 -0500

    completed event listener logic

--- family-tree.js
+++ family-tree.js
@@ -11,7 +11,7 @@ class FamilyTree {
     this.children.push(descendant);
   }
   findMember(name) {
-    if (this.value == name) {
+    if (this.value === name) {
       return this;
     } else {
       for (let i = 0; i < this.children.length; i++) {
@@ -39,4 +39,36 @@ class FamilyTree {
   }
 }
 
-module.exports = FamilyTree;
+var currentNode = null;
+var ancestor;
+
+const input = document.getElementById('input');
+const insert = document.getElementById('insert');
+let head = document.getElementById('head');
+const selectedChild = document.getElementsByClassName('child');
+
+//document.addEventListener('click');
+let dashes = '--';
+
+insert.addEventListener('click', function () {
+  if (input.value === '' || input.value === null || input.value === undefined) {
+    alert('enter a name!');
+  }
+  if (currentNode === null) {
+    ancestor = new FamilyTree(input.value);
+    head.innerHTML += `<div id=${input.value}><button>${input.value}</button></div>`;
+    currentNode = ancestor;
+  } else {
+    currentNode.insert(input.value);
+    head.innerHTML += `<div id=${input.value}>${dashes}<button>${input.value}</button></div>`;
+  }
+  console.log(currentNode);
+});
+
+head.addEventListener('click', function (child) {
+  if (!currentNode.children.includes(child.target.innerHTML)) {
+    dashes += '--';
+  }
+  currentNode = ancestor.findMember(child.target.innerHTML);
+  head = document.getElementById(child.target.innerHTML);
+});
